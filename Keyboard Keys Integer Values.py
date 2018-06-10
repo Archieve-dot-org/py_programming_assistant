89627af0ef914862e98653d0d6c7c08dee5cb8bd
Shaon Majumder <shaon.fdr@robist.com>
2018-06-11 06:37:39 +0700

    within keyboard keypress added

--- Keyboard Keys Integer Values.py
+++ Keyboard Keys Integer Values.py
@@ -2,3 +2,5 @@ from msvcrt import getch
 while True:
     keypress = ord(getch())
     print(keypress)
+    if keypress == 13:
+        break
---
+++ within terminal window.py
@@ -0,0 +1,7 @@
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        # TODO: Get and print the mouse coordinates
+        print("Loop is running. Ctrl + C to quit.")
+except KeyboardInterrupt:
+    print('\nDone.')
